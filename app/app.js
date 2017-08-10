59d8b693521d944390ed1720c3ea1c3504b5673d
aniaw <ania_w@outlook.com>
2017-08-10 13:33:26 +0200

    solution

--- app/app.js
+++ app/app.js
@@ -5,8 +5,17 @@
     //example exercise
 
     window.app = {
-
-
+        getDescendingNumbers: function (start, stop)
+        {
+            if (typeof start !== 'number' || typeof stop !== 'number' || start < stop) {
+                return false;
+            }
+            var descendingNumbers = start;
+            for (var i = start - 1; i >= stop; i--) {
+                descendingNumbers = descendingNumbers + ' ' + i;
+            }
+            return descendingNumbers;
+        }
     };
 
 })();
