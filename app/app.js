70a9793745f5d983fc620d861873a1937ade9f0d
aniaw <ania_w@outlook.com>
2017-08-21 14:58:27 +0200

    solution

--- app/app.js
+++ app/app.js
@@ -2,6 +2,14 @@ var Promise = require('bluebird');
 module.exports = {
     getUsers: function (access)
     {
-
+        var users = [{id: 1001, name: 'Stefan'}, {id: 1002, name: 'John'}];
+        return new Promise(function (resolve, reject)
+        {
+            if (access) {
+                resolve(users)
+            } else {
+                reject('Not permitted!');
+            }
+        })
     }
 };
